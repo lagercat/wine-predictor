4317167c34e16421765dba115ddd1de12ce74a4e
lagercat <malessebastian@gmail.com>
2020-02-14 21:56:40 +0000

    Generate prite statistics

--- nn.py
+++ nn.py
@@ -11,14 +11,24 @@ SEED = 42
 
 np.random.seed(SEED)
 
-raw_dataset = pd.read_csv(DATASET_PATH, header=0)
-labels = raw_dataset.columns.values.tolist()
+raw_dataset = pd.read_csv(DATASET_PATH, header=0).drop(
+        columns=['description', 'region_1', 'Unnamed: 0',
+                 'region_2', 'designation']).dropna()
+
+column_names = raw_dataset.columns.values.tolist()
 
 msk = np.random.rand(len(raw_dataset)) < 0.75
 
 train = raw_dataset[msk]
-test = raw_dataset[~msk]
+train_label = train.pop('points')
 
-print("Labels of the dataset: " + ", ".join(labels))
+test = raw_dataset[~msk]
+test_label = test.pop('points')
+
+print("Labels of the dataset: " + ", ".join(column_names))
 print("Length of the training set " + str(len(train)))
 print("Length of the test set " + str(len(test)))
+
+
+train_stats = train.describe()
+print(train_stats)
