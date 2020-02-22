c10a0dacb409f8fc52df7bb24db95b928c14873c
lagercat <malessebastian@gmail.com>
2020-02-22 15:05:28 +0000

    Improve distribution plotting.

--- src/graphs.py
+++ src/graphs.py
@@ -6,14 +6,24 @@ from matplotlib import pyplot as plt
 DATASET_FILE = 'winemag-data_first150k.csv'
 DATASET_FOLDER = os.environ['WINE_DATASET']
 DATASET_PATH = DATASET_FOLDER + DATASET_FILE
+TAKE_TO_COUNTRY = 10
+TAKE_TO_VARIETY = 20
 current_dir = os.path.dirname(__file__)
 
 dataset = pd.read_csv(DATASET_PATH)
 
 (points, prices, country, variety) = (dataset['points'].tolist(),
                                       dataset['price'].tolist(),
-                                      dataset['country'].tolist(),
-                                      dataset['variety'].tolist())
+                                      dataset['country'],
+                                      dataset['variety'])
+
+most_common_countries = country.value_counts()[:TAKE_TO_COUNTRY].index.to_list()
+country = country.map(lambda x: x if x in most_common_countries else
+'other')
+
+most_common_variety = variety.value_counts()[:TAKE_TO_VARIETY].index.to_list()
+variety = variety.map(lambda x: x if x in most_common_variety else
+'other')
 
 binsForPoints = [x for x in range(80, 101, 2)]
 binsForPrices = [x for x in range(0, 251, 5)]
@@ -32,20 +42,14 @@ plt.title("Distribution of wine Prices")
 plt.savefig(os.path.join(current_dir, '../graphs/prices.png'))
 plt.close()
 
-plt.hist(country, width=1)
-plt.xlabel("Country")
-plt.ylabel("Number")
-plt.title("Distribution of wine's country of origin")
-fig = plt.gcf()
-fig.set_size_inches(28.5, 20.5)
+country.value_counts().plot(kind='barh', figsize=(20, 10),
+                            title='Country Distribution')
 plt.savefig(os.path.join(current_dir, '../graphs/countries.png'), dpi=100)
 plt.close()
 
-plt.hist(variety)
-plt.xlabel("Variety")
-plt.ylabel("Number")
-plt.title("Distribution of wine variety")
-plt.savefig(os.path.join(current_dir, '../graphs/variety.png'))
+variety.value_counts().plot(kind='barh', figsize=(20, 10),
+                            title='Variety Distribution')
+plt.savefig(os.path.join(current_dir, '../graphs/variety.png'), dpi=100)
 plt.close()
 
 plt.plot(prices, points, 'ro')
